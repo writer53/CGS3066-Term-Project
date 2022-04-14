9609b7df69f78fdeb444c9a75414feb763a5adca
writer53 <happyleto@gmail.com>
2022-04-14 17:12:48 -0400

    add carousel code

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,20 @@
-// Writer Leto
-//3/31/2022
+// CGS3066 Term Project
 
+//Image Carousel Code
+var slidePosition = 0;
+SlideShow();
+
+function SlideShow() {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slidePosition++;
+  if (slidePosition > slides.length) {slidePosition = 1}
+  slides[slidePosition-1].style.display = "block";
+  setTimeout(SlideShow, 2000); // Change image every 2 seconds
+} 
 
 //declare variables
 	
